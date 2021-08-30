58e295dbc4d7cea31498dc31fe395c30fd72021f
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2021-08-30 21:36:51 +0800

    Fix passport implementation error

--- library.ts
+++ library.ts
@@ -1,14 +1,14 @@
-import { Auth0Plugin, Database, HostHelpers, PassportCallback, User } from "library";
+import { Auth0Plugin, Database, HostHelpers, PassportCallback, User } from "./library.types";
 
 (function (module: NodeModule) {
 
-  const User: User = require.main.require('./src/user');
-  const db: Database = require.main.require('./src/database');
-  const nconf = require.main.require('nconf');
-  const passport = require.main.require('passport');
-  const winston = require.main.require('winston');
+  const User: User = require.main!.require('./src/user');
+  const db: Database = require.main!.require('./src/database');
+  const nconf = require.main!.require('nconf');
+  const passport = require.main!.require('passport');
+  const winston = require.main!.require('winston');
   const Auth0Strategy = require('passport-auth0');
-	const meta = require.main.require('./src/meta');
+  const meta = require.main!.require('./src/meta');
 
   async function metaSettingsGet<T = any>(key: string) {
     return new Promise<T>((res, rej) => meta.settings.get(key, (err: any, data: T) => err ? rej(err) : res(data)))
@@ -25,7 +25,16 @@ import { Auth0Plugin, Database, HostHelpers, PassportCallback, User } from "libr
   var Auth0: Auth0Plugin = {
     async getStrategy(strategies, callback) {
       try {
-        const settings = await metaSettingsGet<Auth0Plugin["settings"]>("sso-auth0")
+        const settings = await metaSettingsGet<Auth0Plugin["settings"]>("sso-auth0") || {
+          domain: "",
+          id: "",
+          secret: "",
+          superadminRoleId: "",
+          autoAuth0Login: "",
+          disableRegistration: "",
+          displayAssociation: "",
+          preventDeletion: "",
+        }
         Auth0.settings = settings
         winston.verbose("Using Auth0 settings:", Auth0.settings)
 
@@ -38,17 +47,25 @@ import { Auth0Plugin, Database, HostHelpers, PassportCallback, User } from "libr
             passReqToCallback: true,
             state: false,	// this is ok because nodebb core passes state through in .authenticate()
             scope: 'openid email profile',
-          }, <PassportCallback>function (req, token, unused, unused2, profile, done) {
+          }, <PassportCallback>async function (req, accessToken, refreshToken, extraParams, profile, done) {
+            winston.verbose({
+              accessToken,
+              refreshToken,
+              extraParams
+            })
             if (req.hasOwnProperty('user') && req.user.hasOwnProperty('uid') && req.user.uid > 0) {
               // Save Auth0 -specific information to the user
-              Promise.all([
+              await Promise.all([
                 User.setUserField(req.user.uid, 'auth0id', profile.id),
                 db.setObjectField('auth0id:uid', profile.id, req.user.uid),
-              ]).then(() => done(null, req.user))
-            }
+              ])
 
-            var email = Array.isArray(profile.emails) && profile.emails.length ? profile.emails[0].value : '';
-            Auth0.login(profile.id, profile.displayName, email, profile.picture, done);
+              var email = Array.isArray(profile.emails) && profile.emails.length ? profile.emails[0].value : '';
+              const uidInfo = await Auth0.login(profile.id, profile.displayName, email, profile.picture);
+              done(null, uidInfo)
+            } else {
+              done(new Error('[[error:sso-auth0-login-failed, Auth0]]'))
+            }
           }));
 
           strategies.push({
@@ -99,44 +116,42 @@ import { Auth0Plugin, Database, HostHelpers, PassportCallback, User } from "libr
       }
     },
 
-    async login(auth0Id, username, email, picture, callback) {
+    async login(auth0Id, username, email, picture) {
+      if (!Auth0.settings) throw new Error('[[error:sso-plugin-not-yet-loaded, Auth0]]')
       // for auth0 app that does not implement username in login credetials
       if (username === email) {
         username = email.split("@")[0];
       }
+      // TODO: Check if role is superadmin
+      const uid: string | undefined = await Auth0.getUidByAuth0ID(auth0Id)
+      if (uid) return { uid }
+      else {
+        // New User
+        const postRegistration = async function (uid: string) {
+          // trust email returned from Auth0
+          await Promise.all([
+            User.setUserField(uid, 'email:confirmed', 1),
+            db.sortedSetRemove('users:notvalidated', uid),
+            User.setUserField(uid, 'auth0id', auth0Id),
+            User.setUserField(uid, 'uploadedpicture', picture),
+            User.setUserField(uid, 'picture', picture),
+            db.setObjectField('auth0id:uid', auth0Id, uid)
+          ])
+        };
 
-      try {
-        const uid: string | undefined = await Auth0.getUidByAuth0ID(auth0Id)
-        if (uid) callback(null, { uid });
-        else {
-          // New User
-          async function postRegistration(uid: string) {
-            // trust email returned from Auth0
-            await Promise.all([
-              User.setUserField(uid, 'email:confirmed', 1),
-              db.sortedSetRemove('users:notvalidated', uid),
-              User.setUserField(uid, 'auth0id', auth0Id),
-              User.setUserField(uid, 'uploadedpicture', picture),
-              User.setUserField(uid, 'picture', picture),
-              db.setObjectField('auth0id:uid', auth0Id, uid)
-            ])
-            callback(null, {
-              uid: uid
-            });
-          };
-
-          const uid = await User.getUidByEmail(email)
-          if (!uid) {
-            // Abort user creation if registration via SSO is restricted
-            if (Auth0.settings.disableRegistration === 'on') throw new Error('[[error:sso-registration-disabled, Auth0]]')
-            await User.create({ username: username, email: email })
-            postRegistration(uid);
-          } else {
-            postRegistration(uid); // Existing account -- merge
+        const uid = await User.getUidByEmail(email)
+        if (!uid) {
+          // Abort user creation if registration via SSO is restricted
+          if (Auth0.settings.disableRegistration === 'on') throw new Error('[[error:sso-registration-disabled, Auth0]]')
+          const newUid = await User.create({ username: username, email: email })
+          await postRegistration(newUid);
+          return {
+            uid: newUid
           }
+        } else {
+          await postRegistration(uid); // Existing account -- merge
+          return { uid }
         }
-      } catch (err) {
-        callback(err);
       }
     },
 
@@ -153,7 +168,7 @@ import { Auth0Plugin, Database, HostHelpers, PassportCallback, User } from "libr
     },
 
     init(data, callback) {
-      var hostHelpers: HostHelpers = require.main.require('./src/routes/helpers');
+      var hostHelpers: HostHelpers = require.main!.require('./src/routes/helpers');
 
       const renderAdmin: import("express").RequestHandler = (_req, res) => res.render('admin/plugins/sso-auth0', {
         callbackURL: nconf.get('url') + '/auth/auth0/callback'
@@ -169,7 +184,7 @@ import { Auth0Plugin, Database, HostHelpers, PassportCallback, User } from "libr
       });
       data.router.post('/deauth/auth0', [data.middleware.requireUser, data.middleware.applyCSRF], <import("express").RequestHandler>async function (req, res, next) {
         try {
-          await Auth0.deleteUserData({ uid: req.user.uid })
+          await Auth0.deleteUserData({ uid: (req as any).user.uid })
           callback();
         } catch (err) {
           if (err) return next(err);
---
+++ library.types.ts
@@ -0,0 +1,106 @@
+type Strategies = {
+  name: string
+  url: string
+  callbackURL: string
+  icon: string
+  scope: string
+}
+
+type Settings = {
+  domain: string
+  id: string
+  secret: string
+  superadminRoleId: string
+  autoAuth0Login: string
+  disableRegistration: string
+  displayAssociation: string
+  preventDeletion: string
+  [any: string]: string
+}
+
+type WhitelistInfo = {
+  whitelist: string[]
+}
+
+type Callback<T = any> = (err: null | Error, data?: T) => void
+
+export type PassportCallback = (req: Express.Request & { user: { uid: number }}, token: string, _: string, __: string, profile: { id: string, emails?: { value:string }[], displayName: string, picture: string }, done: Callback) => void
+
+type UserAssociations = {
+  uid: string,
+  associations: {
+    associated: boolean,
+    name: string,
+    icon: string,
+    deauthUrl?: string,
+    url?: string,
+  }[]
+}
+
+type CreateUserData = {
+  username: string
+  email: string
+  timestamp?: number
+}
+
+type AsyncVoid = Promise<void>
+type SyncVoid = void
+type CustomHeaders = {
+  authentication: {
+    route: string
+    icon: string
+    name: string
+  }[]
+}
+
+type Router = import("express").Router
+type RequestHandler = import("express").RequestHandler | import("express").ErrorRequestHandler
+
+
+type PluginData = {
+  router: Router
+  middleware: {
+    admin: {
+      [key: string]: RequestHandler
+    }
+    requireUser: RequestHandler
+    applyCSRF: RequestHandler
+  }
+}
+
+export interface Auth0Plugin {
+  getStrategy(strategies: Strategies[], callback: Callback<Strategies[]>): AsyncVoid
+  appendUserHashWhitelist(data: WhitelistInfo, callback: Callback<WhitelistInfo>): SyncVoid
+  getAssociation(user: UserAssociations, callback: Callback<UserAssociations>): AsyncVoid
+  login(auth0Id: string, username: string, email: string, picture: string): Promise<{uid:string}>
+  getUidByAuth0ID(auth0Id: string): Promise<string>
+  addMenuItem(custom_header: CustomHeaders, callback: Callback<CustomHeaders>): SyncVoid
+  init(data: PluginData, callback: () => void): SyncVoid
+  deleteUserData(data: {uid?:string}): Promise<void>
+  authenticateUserPage(data: {res: import("express").Response}): void
+  settings?: Settings
+}
+
+export interface User {
+  getUserField<T extends keyof UserDB>(uid: string | number, key: T): Promise<UserDB[T]>
+  setUserField<T extends keyof UserDB>(uid: string | number, key: T, value: UserDB[T]): Promise<void>
+  getUidByEmail(email: string): Promise<string | undefined>
+  create(data: CreateUserData): Promise<string>
+}
+
+export interface Database {
+  sortedSetRemove(key: string, uid: string | string[]): Promise<void>
+  setObjectField(object: string, key: string, value: any): Promise<void>
+  getObjectField(object: string, key: string): Promise<string>
+}
+
+export interface UserDB {
+  auth0id?: string
+  'email:confirmed'?: number
+  'uploadedpicture': string
+  'picture'?: string
+}
+
+export interface HostHelpers {
+  setupPageRoute(router: Router, path: string, ...middlewares: (PluginData["middleware"] | RequestHandler | RequestHandler[])[]): void
+}
